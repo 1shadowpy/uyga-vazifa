4223b3a2584dd557b35514d117e372245b00440b
1shadowpy <132641739+1shadowpy@users.noreply.github.com>
2023-05-19 10:42:01 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,133 @@
+# otam = {}
+# onam = {}
+# ukam = {}
+#
+#
+# otam['ism']= 'xodjiakbar'
+# otam['yil']= '1983'
+# otam['yosh']= '40'
+#
+# onam['ism']= 'dildora'
+# onam['yil']= '1988'
+# onam['yosh']= '35'
+#
+# ukam['ism']= 'xonazimbek'
+# ukam['yil']= '2014'
+# ukam['yosh']= '9'
+#
+# print(f"meni otamni ismi {otam['ism']},\n neni otamni yili {otam['yil']},\n meni otamni yoshi {otam['yosh']}")
+#
+# print(f"meni omamni ismi {onam['ism']},\n meni onamni yili {onam['yil']},\n meni onamning yoshi {onam['yosh']}")
+#
+# print(f"meni ukamni ismi {ukam['ism']},\n meni ukamni yili {ukam['yil']},\n meni ukamni yoshi {ukam['yosh']}")
+
+
+
+# mamur = {}
+# damir = {}
+# temur = {}
+# komil = {}
+# joha  = {}
+#
+# mamur['taom']= 'somsa'
+# damir['taom']= 'osh'
+# temur['taom']= 'kabob'
+# komil['taom']= 'bosma'
+# joha['taom']= 'monti'
+#
+#
+# print(f" mamurni sevimli taomi {mamur['taom']},\n damirni sevimli taomi bu {damir['taom']},\n temurning sevimli taomi {temur['taom']},\n komilning sevimli taomi bu {komil['taom']},\n johaning sevimli taomi {joha['taom']}")
+#
+
+
+
+
+# python = {}
+#
+# python['if']= 'agar'
+# python['else']= 'yoki'
+# python['upper']= 'katta'
+# python['title']= 'boshqoruvchi'
+# python['elif']= 'agaryoki'
+# python['append']= 'qoshish'
+# python['tab']= 'abzats'
+# python['print']= 'chiqarmoq'
+# print(python)
+#
+#
+# logn = input("soz kiriting:")
+# if logn in python.keys():
+#     print(f"{python.get(f'{logn}')}")
+# else:
+#     print('Bunday so\'z mavjud emas')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
